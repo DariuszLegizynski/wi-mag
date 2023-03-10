7587cfd9b81812e2e25ba80ac670405181b44440
Dariusz Legizynski <dariuszlegizynski@lemon42.com>
2023-03-10 11:37:42 +0100

    add footer component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,13 +3,13 @@ import Hero from '@/components/home/HomeHero.vue'
 // import ProductsPresented from '@/components/home/ProductsPresented.vue'
 // import ProductPros from '@/components/home/ProductPros.vue'
 // import References from '@/components/home/References.vue'
-// import Footer from '@/components/home/Footer.vue'
+import Footer from '@/components/AppFooter.vue'
 </script>
 
 <template>
   <Hero />
   <!-- <ProductsPresented />
   <ProductPros />
-  <References />
-  <Footer /> -->
+  <References /> -->
+  <Footer />
 </template>
