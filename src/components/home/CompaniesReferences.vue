f89780cc067be85ad13ae15b17cdf4a5ccca431e
Dariusz Legizynski <dariuszlegizynski@lemon42.com>
2023-03-14 07:49:25 +0100

    add to more sections

---
+++ src/components/home/CompaniesReferences.vue
@@ -0,0 +1,118 @@
+<script setup>
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+  AOS.init()
+</script>
+
+<template>
+  <article class="references">
+    <h1>Dotychczas zaufali nam:</h1>
+    <section class="references__companies">
+      <img
+        class="references__logo"
+        src="/images/logo/Ikea_logo.svg"
+        alt="logo of ikea"
+        data-aos="fade-up"
+        data-aos-easing="ease-in-sine"
+        data-aos-once="true"
+      />
+      <img
+        class="references__logo"
+        src="/images/logo/Leroy_Merlin.svg"
+        alt="logo Leroy Merlin"
+        data-aos="fade-up"
+        data-aos-easing="ease-in-sine"
+        data-aos-once="true"
+      />
+      <img
+        class="references__logo logo--mrowka"
+        src="/images/logo/PSB-Mrowka.png"
+        alt="logo PSB Mrowka"
+        data-aos="fade-up"
+        data-aos-easing="ease-in-sine"
+        data-aos-once="true"
+      />
+      <img
+        class="references__logo"
+        src="/images/logo/Castorama_Logo.svg"
+        alt="logo Castorama"
+        data-aos="fade-up"
+        data-aos-easing="ease-in-sine"
+        data-aos-once="true"
+      />
+      <img
+        class="references__logo"
+        src="/images/logo/bricoman_logo_3.png"
+        alt="logo Bricoman"
+        data-aos="fade-up"
+        data-aos-easing="ease-in-sine"
+        data-aos-once="true"
+      />
+    </section>
+  </article>
+</template>
+
+
+
+<style lang="scss" scoped>
+.references {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 4rem 1rem;
+
+  & > h1 {
+    margin-bottom: 2rem;
+    padding: 0.4rem;
+    color: $color-primary;
+    text-align: center;
+  }
+
+  &__companies {
+    display: grid;
+    grid-template-columns: 1fr;
+    align-items: center;
+    justify-items: center;
+    row-gap: 4rem;
+  }
+
+  &__logo {
+    width: 8rem;
+    object-fit: contain;
+  }
+}
+.logo--mrowka {
+  width: 4rem;
+}
+
+@media only screen and (min-width: 480px) {
+  .references {
+    &__companies {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 4rem;
+    }
+  }
+}
+@media only screen and (min-width: 1024px) {
+  .references {
+    &__companies {
+      grid-template-columns: repeat(3, 1fr);
+      column-gap: 4rem;
+    }
+  }
+}
+@media only screen and (min-width: 1440px) {
+  .references {
+    padding: 4rem 0 8rem;
+
+    & > h1 {
+      padding: 4rem 0;
+    }
+    &__companies {
+      grid-template-columns: repeat(6, 1fr);
+      column-gap: 4rem;
+    }
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,15 @@
 <script setup>
 import Hero from '@/components/home/HomeHero.vue'
 import ProductGroups from '@/components/home/ProductGroups.vue'
-// import ProductPros from '@/components/home/ProductPros.vue'
-// import References from '@/components/home/References.vue'
+import ProductBenefits from '@/components/home/ProductBenefits.vue'
+import CompaniesReferences from '@/components/home/CompaniesReferences.vue'
 import Footer from '@/components/AppFooter.vue'
 </script>
 
 <template>
   <Hero />
   <ProductGroups />
-  <!-- <ProductPros />
-  <References /> -->
+  <ProductBenefits />
+  <CompaniesReferences />
   <Footer />
 </template>
