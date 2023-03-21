7463bf9f4e98e2cd95f4c946a09988f5ec79cc13
Dariusz Legizynski <dariuszlegizynski@lemon42.com>
2023-03-21 09:43:32 +0100

    ofer view adjusted for laptop screens

--- src/components/offer/ProductCategories.vue
+++ src/components/offer/ProductCategories.vue
@@ -6,12 +6,12 @@ import { collection, onSnapshot } from 'firebase/firestore'
 import { db } from '@/firebase/firebaseInit'
 
 const productCategory = ref([])
-const productId = ref("")
+const productId = ref('')
 onMounted(() => {
   onSnapshot(collection(db, 'productTypes'), (getProductTypes) => {
     getProductTypes.forEach((doc) => {
       productCategory.value.push(doc.data())
-      productId.value = location.hash.replace("#", "")
+      productId.value = location.hash.replace('#', '')
     })
   })
 })
@@ -20,9 +20,18 @@ onMounted(() => {
 <template>
   <ul class="products">
     <li v-for="product in productCategory" :key="product.id">
-      <h1 id="metalowe">{{ product.category }}</h1>
-      <div v-for="productType in product.product_types" :key="productType.id" :id="`${productType.type}`" >
-        <ProductCategory :title="productType.type" :products="productType.product_list" :showCategory="productType.type === productId ? true : false" />
+      <h1>{{ product.category }}</h1>
+      <div
+        v-for="productType in product.product_types"
+        :key="productType.id"
+        :id="`${productType.type}`"
+        class="products__item"
+      >
+        <ProductCategory
+          :title="productType.type"
+          :products="productType.product_list"
+          :showCategory="productType.type === productId ? true : false"
+        />
       </div>
     </li>
   </ul>
@@ -36,9 +45,31 @@ onMounted(() => {
   row-gap: 0.75rem;
   padding: 0rem 0.8rem;
 
-  & h1, & h2, & h3 {
+  & h1,
+  & h2,
+  & h3 {
     color: $color-primary;
     text-transform: capitalize;
+  }
+}
+
+@media (min-width: 480px) {
+  .products {
+    &__item {
+      display: grid;
+      grid-template-columns: repeat(2, minmax(0, 1fr));
+      column-gap: 1rem;
+    }
+  }
+}
+
+@media (min-width: 768px) {
+  .products {
+    &__item {
+      display: grid;
+      grid-template-columns: repeat(3, minmax(0, 1fr));
+      column-gap: 1rem;
+    }
   }
 }
 </style>
